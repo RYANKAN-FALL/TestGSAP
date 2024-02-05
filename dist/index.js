a0e3b47de755c62354bc3847dec8d4e550885b87
RexxL <56723985+RYANKAN-FALL@users.noreply.github.com>
2024-02-05 08:04:19 +0700

    Test

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,60 @@
-"use strict";(()=>{window.Webflow=window.Webflow||[];window.Webflow.push(async()=>{function l(e){let t=document.querySelectorAll('[data-element="cms-item"]'),n=document.querySelectorAll('[data-element="cms-title"]');console.log("CMS Item Elements:",t),console.log("CMS Title Elements:",n),n.forEach((c,s)=>{let o=c.textContent;if(o.includes(e)){let i=t[s];console.log("Title Text:",o),console.log("Corresponding CMS Item:",i)}})}document.querySelector('[data-element="cms-search"]').addEventListener("input",function(){let e=this.value;l(e)})});})();
+"use strict";
+(() => {
+  // bin/live-reload.js
+  new EventSource(`${"http://localhost:3000"}/esbuild`).addEventListener("change", () => location.reload());
+
+  // node_modules/.pnpm/@finsweet+ts-utils@0.40.0/node_modules/@finsweet/ts-utils/dist/webflow/getPublishDate.js
+  var getPublishDate = (page = document) => {
+    const publishDatePrefix = "Last Published:";
+    for (const node of page.childNodes) {
+      if (node.nodeType === Node.COMMENT_NODE && node.textContent?.includes(publishDatePrefix)) {
+        const publishDateValue = node.textContent.trim().split(publishDatePrefix)[1];
+        if (publishDateValue)
+          return new Date(publishDateValue);
+      }
+    }
+  };
+
+  // src/utils/greet.ts
+  var greetUser = (name) => {
+    const publishDate = getPublishDate();
+    console.log(`Hello ${name}!`);
+    console.log(
+      `This site was last published on ${publishDate?.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "2-digit"
+      })}.`
+    );
+  };
+
+  // src/index.ts
+  window.Webflow = window.Webflow || [];
+  window.Webflow.push(async () => {
+    const name = "John Doe";
+    greetUser(name);
+    function searchCMS(titleToSearch) {
+      const itemElements = document.querySelectorAll('[data-element="cms-item"]');
+      const titleElements = document.querySelectorAll('[data-element="cms-title"]');
+      console.log("CMS Item Elements:", itemElements);
+      console.log("CMS Title Elements:", titleElements);
+      const searchQuery = titleToSearch.toLowerCase();
+      titleElements.forEach((titleElement, index) => {
+        const titleText = titleElement.textContent?.toLowerCase();
+        if (titleText && titleText.includes(searchQuery)) {
+          const correspondingItem = itemElements[index];
+          console.log("Title Text:", titleText);
+          console.log("Corresponding CMS Item:", correspondingItem);
+        }
+      });
+    }
+    const searchInput = document.querySelector('[data-element="cms-search"]');
+    if (searchInput) {
+      searchInput.addEventListener("input", function() {
+        const titleToSearch = this.value;
+        searchCMS(titleToSearch);
+      });
+    }
+  });
+})();
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
+import { greetUser } from '$utils/greet';
+
 window.Webflow = window.Webflow || [];
 window.Webflow.push(async () => {
-  function searchCMS(titleToSearch) {
+  const name = 'John Doe';
+  greetUser(name);
+  function searchCMS(titleToSearch: string) {
     const itemElements = document.querySelectorAll('[data-element="cms-item"]');
     const titleElements = document.querySelectorAll('[data-element="cms-title"]');
 
     console.log('CMS Item Elements:', itemElements);
     console.log('CMS Title Elements:', titleElements);
 
-    titleElements.forEach((titleElement, index) => {
-      const titleText = titleElement.textContent;
+    const searchQuery = titleToSearch.toLowerCase(); // Convert the search query to lowercase
 
-      // Check if the current title matches the titleToSearch
-      if (titleText.includes(titleToSearch)) {
+    titleElements.forEach((titleElement, index) => {
+      const titleText = titleElement.textContent?.toLowerCase(); // Use optional chaining to handle possible null
+
+      // Check if the current title matches the searchQuery
+      if (titleText && titleText.includes(searchQuery)) {
         const correspondingItem = itemElements[index];
         console.log('Title Text:', titleText);
         console.log('Corresponding CMS Item:', correspondingItem);
@@ -20,9 +26,11 @@ window.Webflow.push(async () => {
   }
 
   // Add an event listener to the input field with data-element="cms-search"
-  const searchInput = document.querySelector('[data-element="cms-search"]');
-  searchInput.addEventListener('input', function () {
-    const titleToSearch = this.value;
-    searchCMS(titleToSearch);
-  });
+  const searchInput = document.querySelector<HTMLInputElement>('[data-element="cms-search"]'); // Use HTMLInputElement to specify the type
+  if (searchInput) {
+    searchInput.addEventListener('input', function () {
+      const titleToSearch = this.value;
+      searchCMS(titleToSearch);
+    });
+  }
 });
